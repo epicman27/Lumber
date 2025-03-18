200ae1fb496b8df92f227e792de3becaec3447a1
ThatOneLiviRealAccount <Tim@Tims-PC.fritz.box>
2025-03-18 20:11:40 +0100

    StaircaseWhenBlockPlacedBy

--- src/main/java/net/mcreator/lumber/procedures/StaircasePlaceProcedureProcedure.java
+++ src/main/java/net/mcreator/lumber/procedures/StaircasePlaceProcedureProcedure.java
@@ -9,9 +9,7 @@ public class StaircasePlaceProcedureProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (entity.getLookAngle().y >= 0) {
-			world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
-		} else {
+		if (entity.getLookAngle().y > 0) {
 			world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
 		}
 	}
