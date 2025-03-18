71c95ff7cd3385874e6421a104f82e501fc573e2
epicman27 <mailfordiscord123@gmail.com>
2025-03-18 21:14:02 +0100

    -stairs

--- src/main/java/net/mcreator/lumber/block/OakLogStairsBlock.java
+++ src/main/java/net/mcreator/lumber/block/OakLogStairsBlock.java
@@ -1,21 +1,125 @@
 
 package net.mcreator.lumber.block;
 
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.block.state.properties.StairsShape;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
 
-public class OakLogStairsBlock extends StairBlock {
+public class OakLogStairsBlock extends Block implements SimpleWaterloggedBlock {
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+	public static final EnumProperty<AttachFace> FACE = FaceAttachedHorizontalDirectionalBlock.FACE;
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+	public static final EnumProperty<StairsShape> STAIRS_SHAPE = BlockStateProperties.STAIRS_SHAPE;
+
 	public OakLogStairsBlock() {
-		super(() -> Blocks.AIR.defaultBlockState(), BlockBehaviour.Properties.of().ignitedByLava().instrument(NoteBlockInstrument.BASS).sound(SoundType.WOOD).strength(2f));
+		super(BlockBehaviour.Properties.of().ignitedByLava().instrument(NoteBlockInstrument.BASS).sound(SoundType.WOOD).strength(2f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(FACE, AttachFace.WALL).setValue(STAIRS_SHAPE, STAIRS_SHAPE.getValue("straight").get()).setValue(WATERLOGGED, false));
 	}
 
 	@Override
-	public float getExplosionResistance() {
-		return 2f;
+	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+		return state.getFluidState().isEmpty();
 	}
 
 	@Override
-	public boolean isRandomlyTicking(BlockState state) {
-		return false;
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 0;
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return switch (state.getValue(FACING)) {
+			default -> switch (state.getValue(FACE)) {
+				case FLOOR -> Shapes.or(box(0, 8, 0, 16, 16, 8), box(0, 0, 8, 16, 8, 16), box(0, 0, 0, 16, 8, 8));
+				case WALL -> Shapes.or(box(0, 8, 8, 16, 16, 16), box(0, 0, 0, 16, 8, 8), box(0, 8, 0, 16, 16, 8));
+				case CEILING -> Shapes.or(box(0, 0, 0, 16, 8, 8), box(0, 8, 8, 16, 16, 16), box(0, 8, 0, 16, 16, 8));
+			};
+			case NORTH -> switch (state.getValue(FACE)) {
+				case FLOOR -> Shapes.or(box(0, 8, 8, 16, 16, 16), box(0, 0, 0, 16, 8, 8), box(0, 0, 8, 16, 8, 16));
+				case WALL -> Shapes.or(box(0, 8, 0, 16, 16, 8), box(0, 0, 8, 16, 8, 16), box(0, 8, 8, 16, 16, 16));
+				case CEILING -> Shapes.or(box(0, 0, 8, 16, 8, 16), box(0, 8, 0, 16, 16, 8), box(0, 8, 8, 16, 16, 16));
+			};
+			case EAST -> switch (state.getValue(FACE)) {
+				case FLOOR -> Shapes.or(box(0, 8, 0, 8, 16, 16), box(8, 0, 0, 16, 8, 16), box(0, 0, 0, 8, 8, 16));
+				case WALL -> Shapes.or(box(8, 8, 0, 16, 16, 16), box(0, 0, 0, 8, 8, 16), box(0, 8, 0, 8, 16, 16));
+				case CEILING -> Shapes.or(box(0, 0, 0, 8, 8, 16), box(8, 8, 0, 16, 16, 16), box(0, 8, 0, 8, 16, 16));
+			};
+			case WEST -> switch (state.getValue(FACE)) {
+				case FLOOR -> Shapes.or(box(8, 8, 0, 16, 16, 16), box(0, 0, 0, 8, 8, 16), box(8, 0, 0, 16, 8, 16));
+				case WALL -> Shapes.or(box(0, 8, 0, 8, 16, 16), box(8, 0, 0, 16, 8, 16), box(8, 8, 0, 16, 16, 16));
+				case CEILING -> Shapes.or(box(8, 0, 0, 16, 8, 16), box(0, 8, 0, 8, 16, 16), box(8, 8, 0, 16, 16, 16));
+			};
+		};
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(FACING, FACE, STAIRS_SHAPE, WATERLOGGED);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
+		return super.getStateForPlacement(context).setValue(FACE, faceForDirection(context.getNearestLookingDirection())).setValue(FACING, context.getHorizontalDirection().getOpposite()).setValue(STAIRS_SHAPE, STAIRS_SHAPE.getValue("straight").get())
+				.setValue(WATERLOGGED, flag);
+	}
+
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
+	}
+
+	private AttachFace faceForDirection(Direction direction) {
+		if (direction.getAxis() == Direction.Axis.Y)
+			return direction == Direction.UP ? AttachFace.CEILING : AttachFace.FLOOR;
+		else
+			return AttachFace.WALL;
+	}
+
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
+		if (state.getValue(WATERLOGGED)) {
+			world.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+		}
+		return super.updateShape(state, facing, facingState, world, currentPos, facingPos);
 	}
 
 	@Override
