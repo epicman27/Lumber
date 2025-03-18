5603b089b177c172310442672e4c577940649911
ThatOneLiviRealAccount <Tim@Tims-PC.fritz.box>
2025-03-18 21:01:16 +0100

    adjusted StaircasePlaceProcedure

--- src/main/java/net/mcreator/lumber/procedures/StaircasePlaceProcedureProcedure.java
+++ src/main/java/net/mcreator/lumber/procedures/StaircasePlaceProcedureProcedure.java
@@ -1,13 +1,50 @@
 package net.mcreator.lumber.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.BlockPos;
+
+import java.util.Map;
 
 public class StaircasePlaceProcedureProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		if (entity.getLookAngle().y > 0) {
-			world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+			{
+				BlockPos _bp = BlockPos.containing(x, y, z);
+				BlockState _bs = Blocks.AIR.defaultBlockState();
+				BlockState _bso = world.getBlockState(_bp);
+				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+					Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
+					if (_property != null && _bs.getValue(_property) != null)
+						try {
+							_bs = _bs.setValue(_property, (Comparable) entry.getValue());
+						} catch (Exception e) {
+						}
+				}
+				BlockEntity _be = world.getBlockEntity(_bp);
+				CompoundTag _bnbt = null;
+				if (_be != null) {
+					_bnbt = _be.saveWithFullMetadata();
+					_be.setRemoved();
+				}
+				world.setBlock(_bp, _bs, 3);
+				if (_bnbt != null) {
+					_be = world.getBlockEntity(_bp);
+					if (_be != null) {
+						try {
+							_be.load(_bnbt);
+						} catch (Exception ignored) {
+						}
+					}
+				}
+			}
 		}
 	}
 }
