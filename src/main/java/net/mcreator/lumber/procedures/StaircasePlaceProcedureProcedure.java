1e35a4bd8c145882a510d136408f6fa58135fbf7
epicman27 <mailfordiscord123@gmail.com>
2025-03-27 18:32:34 +0100

    nothing

--- src/main/java/net/mcreator/lumber/procedures/StaircasePlaceProcedureProcedure.java
+++ src/main/java/net/mcreator/lumber/procedures/StaircasePlaceProcedureProcedure.java
@@ -3,12 +3,12 @@ package net.mcreator.lumber.procedures;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.core.BlockPos;
 
+import net.mcreator.lumber.init.LumberModBlocks;
 import net.mcreator.lumber.LumberMod;
 
 import java.util.Map;
@@ -21,10 +21,10 @@ public class StaircasePlaceProcedureProcedure {
 		LumberMod.LOGGER.info(entity.getLookAngle().y);
 		temp = Math.atan2(entity.getLookAngle().y, Math.sqrt(Math.pow(entity.getLookAngle().x, 2) + Math.pow(entity.getLookAngle().z, 2)));
 		LumberMod.LOGGER.info(temp);
-		if (temp < 0) {
+		if (temp >= 0) {
 			{
 				BlockPos _bp = BlockPos.containing(x, y, z);
-				BlockState _bs = Blocks.CAVE_AIR.defaultBlockState();
+				BlockState _bs = LumberModBlocks.OAK_LOG_STAIRS_UD.get().defaultBlockState();
 				BlockState _bso = world.getBlockState(_bp);
 				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
 					Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
